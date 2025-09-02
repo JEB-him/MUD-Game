f65a7f19516ede77e9d1c71b3b95f96f1d737445
Streab <2781176466@qq.com>
2025-09-02 15:58:47 +0800

    格式优化

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -31,7 +31,45 @@ std::atomic<bool> keyCaptured(false);
 #include <map>
 
 std::map<int, char> vkToAsciiMap = {
-    {0x30, '0'}, {0x31, '1'}, {0x32, '2'}, {0x33, '3'}, {0x34, '4'}, {0x35, '5'}, {0x36, '6'}, {0x37, '7'}, {0x38, '8'}, {0x39, '9'}, {0x41, 'a'}, {0x42, 'b'}, {0x43, 'c'}, {0x44, 'd'}, {0x45, 'e'}, {0x46, 'f'}, {0x47, 'g'}, {0x48, 'h'}, {0x49, 'i'}, {0x4A, 'j'}, {0x4B, 'k'}, {0x4C, 'l'}, {0x4D, 'm'}, {0x4E, 'n'}, {0x4F, 'o'}, {0x50, 'p'}, {0x51, 'q'}, {0x52, 'r'}, {0x53, 's'}, {0x54, 't'}, {0x55, 'u'}, {0x56, 'v'}, {0x57, 'w'}, {0x58, 'x'}, {0x59, 'y'}, {0x5A, 'z'}, {VK_SPACE, ' '}, {VK_RETURN, '\n'}, {VK_ESCAPE, 27}};
+    {0x30, '0'},
+    {0x31, '1'},
+    {0x32, '2'},
+    {0x33, '3'},
+    {0x34, '4'},
+    {0x35, '5'},
+    {0x36, '6'},
+    {0x37, '7'},
+    {0x38, '8'},
+    {0x39, '9'},
+    {0x41, 'a'},
+    {0x42, 'b'},
+    {0x43, 'c'},
+    {0x44, 'd'},
+    {0x45, 'e'},
+    {0x46, 'f'},
+    {0x47, 'g'},
+    {0x48, 'h'},
+    {0x49, 'i'},
+    {0x4A, 'j'},
+    {0x4B, 'k'},
+    {0x4C, 'l'},
+    {0x4D, 'm'},
+    {0x4E, 'n'},
+    {0x4F, 'o'},
+    {0x50, 'p'},
+    {0x51, 'q'},
+    {0x52, 'r'},
+    {0x53, 's'},
+    {0x54, 't'},
+    {0x55, 'u'},
+    {0x56, 'v'},
+    {0x57, 'w'},
+    {0x58, 'x'},
+    {0x59, 'y'},
+    {0x5A, 'z'},
+    {VK_SPACE, ' '},
+    {VK_RETURN, '\n'},
+    {VK_ESCAPE, 27}};
 
 int vkToAscii(int vkCode)
 {
@@ -55,7 +93,7 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
             KBDLLHOOKSTRUCT *pKeyBoard = (KBDLLHOOKSTRUCT *)lParam;
             capturedKey = vkToAscii(pKeyBoard->vkCode);
             keyCaptured = true;
-            std::cout << "Key pressed: " << (char)capturedKey << std::endl;
+            // std::cout << "Key pressed: " << (char)capturedKey << std::endl;
         }
     }
 
@@ -67,8 +105,6 @@ int waitKeyDown()
 {
     capturedKey = -1;
     keyCaptured = false;
-
-    std::cout << "Waiting key down ..." << std::endl;
 
     /* 设置低级键盘钩子 */
     hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, GetModuleHandle(NULL), 0);
@@ -166,18 +202,19 @@ int main()
     while (1)
     {
         int key = waitKeyDown();
-
-        // 检查ESC键退出
         if (key == 27)
         {
-            std::cout << " ESC pressed.";
+            // std::cout << " ESC pressed.";
             break;
         }
-        if (key != -1)
-        {
-            std::cout << "Captured key code: " << std::dec << key << " " << (char)key << std::endl;
-        }
+        //     // 检查输出
+        //     if (key != -1)
+        //     {
+        //         std::cout << "Captured key code: " << std::dec << key << " " << (char)key << std::endl;
+        //     }
+        // }
     }
-    std::cout << " Exit execution." << std::endl;
+    // std::cout << " Exit execution." << std::endl;
+
     return 0;
 }

698d5f88c767b95413b5d02982d462698372986f
Cheongfan <wuxiaotian@stu.ouc.edu.cn>
2025-09-11 14:52:03 +0800

    背包类注释修改，补丁：修复消耗品使用后可能的未释放的问题

--- src/backpack.h
+++ src/backpack.h
@@ -2,6 +2,9 @@
 * @file backpack‌.h
 * @author Xiaotian Wu(Cheongfan)
 * @brief 背包类
+* @details 实现功能：物品的获得、读取与使用。 \n
+* 对应接口：addItem、getBackpack‌Items、useFunctionOfItem
+* @note 与物品相关的一切操作与背包类交互即可，无需关注物品类。
 * */
 
 #pragma once
@@ -22,18 +25,14 @@ using std::vector;
 /**
  * @brief 背包类
  * @param item_creator 物品生成器
- * @param backpack‌_space 背包空间，储存所有物品对象的unique智能指针
- * @param num_items 当前背包物品数量
- * @param capacity  背包最大容量
+ * @param backpack_items 背包空间，储存所有物品对象的unique智能指针
  */
 class Backpack {
 public:
 
 	/**
 	 * @brief 背包类构造函数
-	 * @param capacity 设置最大容量
 	 * @details vector初始容量设定为10，容量超限则扩容10倍
-	 * @details num_items初始值为0
 	 */
 	Backpack();
 
@@ -42,18 +41,25 @@ public:
 	 */
 	~Backpack() = default;
 
+	/**
+	 * @brief 获取背包管理物品智能指针的vector数组
+	 * @return vector数组的引用
+	 */
 	vector<unique_ptr<Item>>& getBackpack‌Items();
 
 	/**
-	 * @brief  
-	* @details 利用ItemCreator的方法创造目标物品对象，存入backpack‌_space中，num_items++
-	*/
+	 * @brief  添加（生成）名为“item_name”的物品
+	 * @param item_name 物品的名称
+	 * @details 逻辑：利用ItemCreator的方法创造目标物品对象，存入backpack_items中
+	 */
 	void addItem(string item_name);
 
 	/**
 	 * @brief 使用或装备或卸下物品(三合一功能函数)
-	 * @details 区分好物品类型和装备状态，然后调用Item正确的接口(use、equipAndUnequip二选一)
-	 * @details 消耗品在调用此方法后，指针释放，num_items--，然后序号在此物品之后的物品指针均前移一个位置
+	 * @param order 操作的物品的序号(从1开始)
+	 * @param protagonist 传入主角对象名
+	 * @details 逻辑：区分好物品类型和装备状态，然后调用Item正确的接口(use、equipAndUnequip二选一)
+	 * @details 逻辑：消耗品在调用此方法后，指针释放，num_items--，然后序号在此物品之后的物品指针均前移一个位置
 	 */
 	void useFunctionOfItem(int order, Protagonist& protagonist);
 

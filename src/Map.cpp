c7b4b26fb18ee5b5900dd129e22313c001dc460a
iamsleepyat8 <Thz14159@163.com>
2025-09-16 12:24:02 +0800

    fix:地图路径修复

--- src/Map.cpp
+++ src/Map.cpp
@@ -224,7 +224,7 @@ Message Map::loadMap(const std::string& filename) {
     // 检查文件路径
     for (const auto& ch : filename)
         if (ch == '/' || ch == '\\') return {"非法文件名", -1};
-    map_path = Controller::getInstance()->getRootDir() / filename;
+    map_path = (Controller::getInstance()->getRootDir() / filename).string();
     bool return_is_valid = false;
 
     // 获取当前路径
